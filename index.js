6169a90aceb08439047710960e8a6fed40b48174
Bradigson <bradigson@gmail.com>
2022-10-24 12:35:09 -0400

    test

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.json());
+
+app.use("/", (req, res)=>{
+    res.json({message : "hellow world"})
+})
+
+
+app.listen(app.get("port"), ()=>{
+    console.log("server available at", app.get("port"))
+})
